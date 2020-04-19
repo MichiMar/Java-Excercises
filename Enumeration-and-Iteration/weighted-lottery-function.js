2af933ad692f23dd58f27d0b7f08b26ce2cb5f4e
MichiMar <michimar.alexis@gmail.com>
2020-04-19 18:33:35 -0500

    weighted lottery funtion

---
+++ Enumeration-and-Iteration/weighted-lottery-function.js
@@ -0,0 +1,21 @@
+const weightedLottery = (weights) => {
+  let containerArray = [];
+
+  Object.keys(weights).forEach((key) => {
+    for (let i = 0; i < weights[key]; i++) {
+      containerArray.push(key);
+    }
+  });
+
+  return containerArray[(Math.random() * containerArray.length) | 0];
+};
+
+const weights = {
+  green: 1,
+  yellow: 2,
+  red: 3,
+};
+
+weightedLottery(weights);
+
+// JavaScript coding excercises / enumeration-iteraction / Build a Weighted Lottery Function in JavaScript
