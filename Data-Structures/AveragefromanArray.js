d2e72d3565b857cefa531850d6b9be31b3d7f8e8
MichiMar <michimar.alexis@gmail.com>
2020-04-21 15:41:52 -0500

    Average Excercise

---
+++ Data-Structures/AveragefromanArray.js
@@ -0,0 +1,9 @@
+const getAverage = (arr) => {
+  const reducer = (total, currentValue) => total + currentValue;
+  const total = arr.reduce(reducer);
+  return total / arr.length;
+};
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+getAverage(arr);
