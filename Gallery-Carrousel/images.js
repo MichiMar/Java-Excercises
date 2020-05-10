c0c7a8a781c8332e7e5fc30a0b122ce4ed0d5f3b
MichiMar <michimar.alexis@gmail.com>
2020-05-09 23:53:42 -0500

    added images and fixed names

---
+++ Gallery-Carrousel/images.js
@@ -0,0 +1,7 @@
+export const images = [
+  "https://images.unsplash.com/photo-1550475476-44c382c5f2a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+  "https://images.unsplash.com/photo-1453749024858-4bca89bd9edc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=707&q=80",
+  "https://images.unsplash.com/photo-1502781252888-9143ba7f074e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80",
+  "https://images.unsplash.com/photo-1489367874814-f5d040621dd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=729&q=80",
+  "https://images.unsplash.com/photo-1572727073081-3151c981197e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=737&q=80",
+];
